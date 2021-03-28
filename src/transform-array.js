766c26da880070a87d761a645f100dbe057cbe38
Alexandr Lazar <lazarforcode@gmail.com>
2021-03-29 00:24:47 +0300

    feat: completed several tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(!Array.isArray(arr)){
+    throw new Error('THROWN');
+  }
+  let arr2 = arr.slice();
+  let arr3 = [];
+  let flag = true;
+  for(let i = 0; i < arr2.length; i++){
+    if(arr2[i] === "--discard-next"){
+      if(i != arr2.length-1){
+        flag = false;
+        i = i+1;
+      }
+    }else if(arr2[i] === "--discard-prev"){
+      if(i != 0 && flag){
+        arr3.pop();
+      }
+    }else if(arr2[i] === "--double-next"){
+      if(i != arr2.length-1){
+        arr3.push(arr2[i+1]);
+      }
+    }else if(arr2[i] === "--double-prev"){
+      if(i != 0 && flag){
+          arr3.push(arr3[arr3.length-1]);
+      }
+    }else{
+      arr3.push(arr2[i]);
+      flag = true;
+    }
+  }
+  return arr3;
 };
