c37e01d1f88d1f9cbfffab758ea1fd0313d37b88
Alexandr Lazar <lazarforcode@gmail.com>
2021-09-09 01:13:45 +0300

    feat: compleat dream-team, hanoi-tower & transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,35 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  let arr2 = arr.slice();
+  let arr3 = [];
+  let flag = true;
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] === "--discard-next") {
+      if (i != arr2.length - 1) {
+        flag = false;
+        i = i + 1;
+      }
+    } else if (arr2[i] === "--discard-prev") {
+      if (i != 0 && flag) {
+        arr3.pop();
+      }
+    } else if (arr2[i] === "--double-next") {
+      if (i != arr2.length - 1) {
+        arr3.push(arr2[i + 1]);
+      }
+    } else if (arr2[i] === "--double-prev") {
+      if (i != 0 && flag) {
+        arr3.push(arr3[arr3.length - 1]);
+      }
+    } else {
+      arr3.push(arr2[i]);
+      flag = true;
+    }
+  }
+  return arr3;
 }
