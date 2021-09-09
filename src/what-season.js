845d97e8f8d97cc3687cba5e96207912736d8e7b
Alexandr Lazar <lazarforcode@gmail.com>
2021-09-09 21:30:08 +0300

    feat: fulfilled carbon-dating, simple-chain & what-season tasks

--- src/what-season.js
+++ src/what-season.js
@@ -14,27 +14,29 @@ import { NotImplementedError } from '../extensions/index.js';
 export default function getSeason(date) {
   if (date == null) {
     return 'Unable to determine the time of year!'
-  } else if (Object.prototype.toString.call(date) !== "[object Date]") {
+  }
+  try {
+    date.toDateString();
+  } catch (e) {
     throw new Error("Invalid date!");
-  } else {
-    let month = date.getMonth();
-    switch (month) {
-      case 11:
-      case 0:
-      case 1:
-        return "winter";
-      case 2:
-      case 3:
-      case 4:
-        return "spring";
-      case 5:
-      case 6:
-      case 7:
-        return "summer";
-      case 8:
-      case 9:
-      case 10:
-        return "fall";
-    }
+  }
+  let month = date.getMonth();
+  switch (month) {
+    case 11:
+    case 0:
+    case 1:
+      return "winter";
+    case 2:
+    case 3:
+    case 4:
+      return "spring";
+    case 5:
+    case 6:
+    case 7:
+      return "summer";
+    case 8:
+    case 9:
+    case 10:
+      return "fall";
   }
 }
