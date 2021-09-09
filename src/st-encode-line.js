adab2e82b8149954ea8fbd523a530690b5949908
Alexandr Lazar <lazarforcode@gmail.com>
2021-09-10 00:03:46 +0300

    feat: filfilled recursive-depth task

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,15 +10,7 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(str) {
-  let arr = str.split("");
-  let obj = {};
-  for (let i = 0; i < arr.length; i++) {
-    if (obj[arr[i]] == null) {
-      obj[arr[i]] = 1;
-    } else {
-      obj[arr[i]] = obj[arr[i]] + 1;
-    }
-  }
-  return Object.entries(obj).map(item => item.reverse()).flat(100).join("");
+export default function encodeLine(/* str */) {
+  throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
 }
