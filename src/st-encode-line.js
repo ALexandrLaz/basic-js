96bda28419ab744987dec45898727b8b72557a9b
Alexandr Lazar <lazarforcode@gmail.com>
2021-09-29 20:45:04 +0300

    feat: add a dns-stats & mine-sweeper task

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,20 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  const arr = str.split("");
+  let resStr = "";
+  let count = 1;
+  for(let i = 0; i < arr.length; i++){
+      if(arr[i] == arr[i + 1]){
+        count++;
+      }else if(count > 1){
+        resStr += `${count + arr[i]}`;
+        count = 1;
+      }else{
+        resStr += `${arr[i]}`;
+        count = 1;
+      }
+  }
+  return resStr
 }
