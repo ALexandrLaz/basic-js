b7cb9f228aab37b40a23e5ea594899da7f1755cf
Alexandr Lazar <lazarforcode@gmail.com>
2021-10-04 23:01:31 +0300

    feat: add a last task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,10 +20,63 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
+  constructor(type){
+    this.type = false;
+    if(type === false){
+      this.type = true;
+    };
+    this.start = 65;
+    this.end = 90;
+  }
+  criptMessage(message, key){
+    return message == key ? key.toUpperCase() : key.repeat(Math.round(message.length / key.length)).slice(0, message.length).toUpperCase();
+  }
   encrypt(message, key) {
-    if(message == undefined || key == undefined) throw new Error('Incorrect arguments');
+    if(!message || !key) throw new Error('Incorrect arguments!');
+    let criptoMessage = this.criptMessage(message, key);
+    let upperMessage = message.toUpperCase();
+    let result = [];
+    for (let i = 0, j = 0; i < upperMessage.length; i++) {
+      let letterPos = upperMessage.charCodeAt(i);
+
+      if (letterPos >= this.start && letterPos <= this.end) {
+        let newMesPos = letterPos - this.start;
+        let newKeyPos = criptoMessage.charCodeAt(j) - this.start;
+
+        let encode = this.start + ((newMesPos + newKeyPos) % 26);
+        let decode = String.fromCharCode(encode);
+
+        result.push(decode)
+        j++
+      } else {
+        result.push(upperMessage[i])
+      }
+    }
+
+    return this.type ? result.reverse().join('') : result.join('')
   }
   decrypt(message, key) {
-    if(message == undefined || key == undefined) throw new Error('Incorrect arguments');
+    if(!message || !key) throw new Error('Incorrect arguments!');
+    let criptoMessage = this.criptMessage(message, key);
+    let upperMessage = message.toUpperCase();
+    let result = [];
+    for (let i = 0, j = 0; i < upperMessage.length; i++) {
+      let letterPos = upperMessage.charCodeAt(i);
+
+      if (letterPos >= this.start && letterPos <= this.end) {
+        let newMesPos = letterPos - this.start;
+        let newKeyPos = criptoMessage.charCodeAt(j) - this.start;
+
+        let encode = this.start + ((newMesPos - newKeyPos + 26) % 26);
+        let decode = String.fromCharCode(encode);
+
+        result.push(decode)
+        j++
+      } else {
+        result.push(upperMessage[i])
+      }
+    }
+
+    return this.type ? result.reverse().join('') : result.join('')
   }
 }
